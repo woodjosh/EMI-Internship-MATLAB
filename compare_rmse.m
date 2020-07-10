84d5b567334c455d4eb870d68652818e20974b1f
woodjosh <woodjosh@umich.edu>
2020-07-10 12:36:10 -0400

    updated to include time vs error plot

--- compare_rmse.m
+++ compare_rmse.m
@@ -1,5 +1,5 @@
 models = ["waffleADX","waffle3DM","waffleEG120","waffleEG1300"]; 
-location = "emptyroom"; 
+location = "house"; 
 
 gyro_data(1) = load(sprintf("/home/josh/Matlab/Datasets/%s_%s.mat",location,models(1))); 
 gyro_data(2) = load(sprintf("/home/josh/Matlab/Datasets/%s_%s.mat",location,models(2))); 
@@ -16,17 +16,24 @@ y_vis = [rmse.y_vis];
 x_imu = [rmse.x_imu]; 
 y_imu = [rmse.y_imu];
 
+x_ts_vio = [rmse.x_ts_vio];
+y_ts_vio = [rmse.y_ts_vio];
+x_ts_vis = [rmse.x_ts_vis];
+y_ts_vis = [rmse.y_ts_vis];
+x_ts_imu = [rmse.x_ts_imu];
+y_ts_imu = [rmse.y_ts_imu];
+
+
 close all; 
 
-figure('Position',[500 500 1200 400]); 
-tiledlayout(1,2) % Requires R2019b or later
-title('House Environment'); 
+figure('Position',[500 600 1200 400]); 
+tiledlayout(1,2) 
 
 nexttile
     hold on; 
     scatter(x_axis,x_vio,'m'); 
     scatter(x_axis,x_imu,'b'); 
-    %scatter(x_axis,x_vis,'r');  
+    scatter(x_axis,x_vis,'r');  
     set(gca,'xtick',x_axis);
     set(gca,'xticklabel',x_labels,'fontsize',8)
     axis([1 4 0 3]); 
@@ -38,12 +45,104 @@ nexttile
     hold on; 
     scatter(x_axis,y_vio,'m'); 
     scatter(x_axis,y_imu,'b'); 
-    %scatter(x_axis,y_vis,'r'); 
+    scatter(x_axis,y_vis,'r'); 
     set(gca,'xtick',x_axis);
     set(gca,'xticklabel',x_labels,'fontsize',8)
-    axis([1 4 0 3]); 
+    axis([1 4 0 5]); 
     legend('Visual+Inertial Fused','Inertial','Visual','Location','northwest'); 
     ylabel('Y RMSE'); 
     hold off; 
+    
+figure('Position',[0 50 2400 400]); 
+tiledlayout(1,4) 
 
+nexttile
+    hold on; 
+    plot(x_ts_vio(1),'m'); 
+    plot(x_ts_imu(1),'b'); 
+    plot(x_ts_vis(1),'r'); 
+    axis([0 45 0 5]); 
+    legend('Visual+Inertial Fused','Inertial','Visual','Location','northwest'); 
+    ylabel('X RMSE'); xlabel('Time (s)'); 
+    title(models(1)); 
+    hold off; 
+    
+nexttile 
+    hold on; 
+    plot(x_ts_vio(2),'m'); 
+    plot(x_ts_imu(2),'b'); 
+    plot(x_ts_vis(2),'r'); 
+    axis([0 45 0 5]); 
+    legend('Visual+Inertial Fused','Inertial','Visual','Location','northwest'); 
+    ylabel('X RMSE'); xlabel('Time (s)'); 
+    title(models(2)); 
+    hold off; 
+
+nexttile    
+    hold on; 
+    plot(x_ts_vio(3),'m'); 
+    plot(x_ts_imu(3),'b'); 
+    plot(x_ts_vis(3),'r'); 
+    axis([0 45 0 5]); 
+    legend('Visual+Inertial Fused','Inertial','Visual','Location','northwest'); 
+    ylabel('X RMSE'); xlabel('Time (s)'); 
+    title(models(3)); 
+    hold off; 
+    
+nexttile    
+    hold on; 
+    plot(x_ts_vio(4),'m'); 
+    plot(x_ts_imu(4),'b'); 
+    plot(x_ts_vis(4),'r'); 
+    axis([0 45 0 5]); 
+    legend('Visual+Inertial Fused','Inertial','Visual','Location','northwest'); 
+    ylabel('X RMSE'); xlabel('Time (s)'); 
+    title(models(4)); 
+    hold off; 
+figure('Position',[0 50 2400 400]); 
+tiledlayout(1,4) 
+
+ nexttile
+    hold on; 
+    plot(y_ts_vio(1),'m'); 
+    plot(y_ts_imu(1),'b'); 
+    plot(y_ts_vis(1),'r'); 
+    axis([0 45 0 5]); 
+    legend('Visual+Inertial Fused','Inertial','Visual','Location','northwest'); 
+    ylabel('Y RMSE'); xlabel('Time (s)'); 
+    title(models(1)); 
+    hold off; 
+    
+nexttile 
+    hold on; 
+    plot(y_ts_vio(2),'m'); 
+    plot(y_ts_imu(2),'b'); 
+    plot(y_ts_vis(2),'r'); 
+    axis([0 45 0 5]); 
+    legend('Visual+Inertial Fused','Inertial','Visual','Location','northwest'); 
+    ylabel('Y RMSE'); xlabel('Time (s)'); 
+    title(models(2)); 
+    hold off; 
+
+nexttile    
+    hold on; 
+    plot(y_ts_vio(3),'m'); 
+    plot(y_ts_imu(3),'b'); 
+    plot(y_ts_vis(3),'r'); 
+    axis([0 45 0 5]); 
+    legend('Visual+Inertial Fused','Inertial','Visual','Location','northwest'); 
+    ylabel('Y RMSE'); xlabel('Time (s)'); 
+    title(models(3)); 
+    hold off; 
+    
+nexttile    
+    hold on; 
+    plot(y_ts_vio(4),'m'); 
+    plot(y_ts_imu(4),'b'); 
+    plot(y_ts_vis(4),'r'); 
+    axis([0 45 0 5]); 
+    legend('Visual+Inertial Fused','Inertial','Visual','Location','northwest'); 
+    ylabel('Y RMSE'); xlabel('Time (s)'); 
+    title(models(4)); 
+    hold off; 
     
--- process_rosbag.m
+++ process_rosbag.m
@@ -3,8 +3,8 @@
 
 %% Parameters, change these to match those used to record data
 models = ["waffleADX","waffle3DM","waffleEG120","waffleEG1300"]; 
-location = "emptyroom"; 
-have_vis = false; 
+location = "house"; 
+have_vis = true; 
 
 for model = models
     %% Select topic from rosbag and import it as a timeseries 
@@ -18,16 +18,22 @@ for model = models
         
     suffix = ["gnd","vio","imu","vis"]; 
     ts = struct; 
-
-    ts.(suffix(1)) = timeseries(rosbag_gnd,'Pose.Pose.Position.X','Pose.Pose.Position.Y'); 
+    
+    ts.(suffix(1)) = timeseries(rosbag_gnd,'Pose.Pose.Position.X','Pose.Pose.Position.Y');  
     ts.(suffix(2)) = timeseries(rosbag_vio,'Pose.Pose.Position.X','Pose.Pose.Position.Y'); 
     ts.(suffix(3)) = timeseries(rosbag_imu,'Pose.Pose.Position.X','Pose.Pose.Position.Y'); 
     if have_vis
-        ts.(suffix(4)) = timeseries(rosbag_vis,'Pose.Pose.Position.X','Pose.Pose.Position.Y'); 
+        ts.(suffix(4)) = timeseries(rosbag_vis,'Pose.Pose.Position.X','Pose.Pose.Position.Y');  
     else 
         ts.(suffix(4)) = timeseries(zeros(1,10),ones(1,10)*ts.(suffix(1)).Time(1)); 
     end
-
+    
+    %% Adjust time so it starts at 0
+    start = ts.(suffix(1)).Time(1); 
+    ts.(suffix(1)).Time = ts.(suffix(1)).Time-start;
+    ts.(suffix(2)).Time = ts.(suffix(2)).Time-start;
+    ts.(suffix(3)).Time = ts.(suffix(3)).Time-start; 
+    ts.(suffix(4)).Time = ts.(suffix(4)).Time-start;  
     %% Clear temporary variables
     clearvars -except ts suffix model location have_vis
 
@@ -42,6 +48,8 @@ for model = models
     for s = suffix(2:size(suffix,2)) 
         [rmse.(sprintf('x_%s',s)),rmse.(sprintf('y_%s',s))] ... 
             = calc_rmse_xy(ts.(sprintf('%s_resample',s)), ts.(sprintf('gnd_resample_%s',s))); 
+        [rmse.(sprintf('x_ts_%s',s)),rmse.(sprintf('y_ts_%s',s))] ... 
+            = calc_rmse_ts_xy(ts.(sprintf('%s_resample',s)), ts.(sprintf('gnd_resample_%s',s))); 
     end 
 
     print_rmse(suffix,rmse); 
@@ -65,7 +73,7 @@ for model = models
             plot(ts.(suffix(i)).Time,ts.(suffix(i)).Data(:,1),colors(i),'LineWidth',5); 
         end  
         title('X Position vs Time'); 
-        xlabel('Time'); ylabel('X Position'); 
+        xlabel('Time (s)'); ylabel('X Position'); 
         legend('Ground Truth','Visual+Inertial Fused','Inertial','Visual','Location','northwest'); 
     hold off; 
 
@@ -75,7 +83,7 @@ for model = models
             plot(ts.(suffix(i)).Time,ts.(suffix(i)).Data(:,2),colors(i),'LineWidth',5); 
         end 
         title('Y Position vs Time'); 
-        xlabel('Time'); ylabel('Y Position'); 
+        xlabel('Time (s)'); ylabel('Y Position'); 
         legend('Ground Truth','Visual+Inertial Fused','Inertial','Visual','Location','northwest'); 
     hold off; 
 
@@ -84,9 +92,18 @@ for model = models
     save(sprintf("/home/josh/Matlab/Datasets/%s_%s.mat",location,model))
 end
 %% Helper functions
+function [rmse_ts_x, rmse_ts_y] = calc_rmse_ts_xy(est, gnd)
+    rmse_ts_x = timeseries; 
+    rmse_ts_y = timeseries; 
+    for time = 1:size(est.Time) 
+        rmse_ts_x = addsample(rmse_ts_x,'Data',calc_rmse(est.Data(1:time,1),gnd.Data(1:time,1)),'Time',est.Time(time));
+        rmse_ts_y = addsample(rmse_ts_y,'Data',calc_rmse(est.Data(1:time,2),gnd.Data(1:time,2)),'Time',est.Time(time));
+    end
+end 
+
 function [rmse_x, rmse_y] = calc_rmse_xy(est, gnd)
-    rmse_x = calc_rmse(est.Data(:,1),gnd.Data(:,1));
-    rmse_y = calc_rmse(est.Data(:,2),gnd.Data(:,2));
+    rmse_x = calc_rmse(est.Data(1:end,1),gnd.Data(1:end,1));
+    rmse_y = calc_rmse(est.Data(1:end,2),gnd.Data(1:end,2));
 end
 
 function rmse = calc_rmse(est,gnd)
