cfa0350861fe6e8462f676d528e3925de7b3e7eb
woodjosh <woodjosh@umich.edu>
2020-07-27 09:52:13 -0400

    added ability to reselect region when visually tracking the turtlebot

--- track_turtlebot.m
+++ track_turtlebot.m
@@ -9,34 +9,68 @@ videoPlayer = vision.VideoPlayer('Position',[100,100,680,520]);
 % Read the first video frame, which contains the object, define the region.
 objectFrame = readFrame(videoReader);
 
-% Get user input to define region and scale
-figure('WindowState','fullscreen'); imshow(objectFrame);  
-title("Draw box around object to track"); 
-objectRegion=round(getPosition(imrect));
-close; 
+tryagain = true; 
+
+% keep trying to get user input as long as user wants
+while tryagain
+    close all; 
+    % Get user input to define region and scale
+    figure('WindowState','fullscreen'); imshow(objectFrame);  
+    title("Draw box around object to track"); 
+    objectRegion=round(getPosition(imrect));
+    close; 
+    % Show initial frame with a red bounding box.
+    objectImage = insertShape(objectFrame,'Rectangle',objectRegion,'Color','red'); 
+    figure;
+
+    imshow(objectImage);
+    title('Red box shows object region');
+
+    % Detect interest points in the object region.
+    points = detectMinEigenFeatures(rgb2gray(objectFrame),'ROI',objectRegion);
+
+    % Display the detected points.
+    pointImage = insertMarker(objectFrame,points.Location,'+','Color','white');
+    pos = mean(points.Location(:,:)); 
+    pointImage = insertMarker(pointImage,pos,'x','Color','blue','Size',5); 
+    figure;
+    imshow(pointImage);
+    title('Detected interest points');
+    
+    % Ask user if the interest points are acceptable 
+    f = figure;
+    c = uicontrol;
+    c.String = 'Yes';
+    c.Callback = @yesButton;
+    c.Position = [200 200 100 100];
+    
+    d = uicontrol; 
+    d.String = 'No'; 
+    d.Callback = @noButton; 
+    d.Position = [300 200 100 100]; 
+    
+    annotation('textbox', [0.28 0.8 0 0], 'String', 'Are the detected interest points correct?', 'FitBoxToText', true);
+    uiwait(f);  
+ 
+end
+% callback functions for button push 
+        function yesButton(src,event)
+            tryagain = false; 
+            uiresume; 
+            close all; 
+        end
+    
+        function noButton(src,event)
+            tryagain = true; 
+            uiresume; 
+        end
+
 figure('WindowState','fullscreen'); imshow(objectFrame); 
-title("Draw line across white circle"); 
+title("Draw line across white circle for scale"); 
 scale=round(getPosition(imline));
-fprintf("%3.1f pixels = .1 m\n",pdist(scale)); 
 scale = pdist(scale); 
 close; 
-% Show initial frame with a red bounding box.
-objectImage = insertShape(objectFrame,'Rectangle',objectRegion,'Color','red'); 
-figure;
 
-imshow(objectImage);
-title('Red box shows object region');
-
-% Detect interest points in the object region.
-points = detectMinEigenFeatures(rgb2gray(objectFrame),'ROI',objectRegion);
-
-% Display the detected points.
-pointImage = insertMarker(objectFrame,points.Location,'+','Color','white');
-pos = mean(points.Location(:,:)); 
-pointImage = insertMarker(pointImage,pos,'x','Color','blue','Size',5); 
-figure;
-imshow(pointImage);
-title('Detected interest points');
  
 % Create a tracker object.
 tracker = vision.PointTracker('MaxBidirectionalError',1);
